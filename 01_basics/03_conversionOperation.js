f2c2e7e080f22f22bca4cfe63e466091be764b8e
Nanak <166646358+iamNanak@users.noreply.github.com>
2024-06-18 13:50:01 +0000

    type conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+// let score = "99"         //Output => 99  
+// let score = "99abc"      //Output => Nan
+// let score = null;        //Output => 0
+// let score = undefined;   //Output => NaN
+// let score = true;        //Output => 1 and 0 for false
+let score = "Nanak";        //Output => NaN
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
