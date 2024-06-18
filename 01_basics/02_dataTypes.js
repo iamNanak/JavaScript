053bbcdc62e97edcb029e013caf52876b646985e
Nanak <166646358+iamNanak@users.noreply.github.com>
2024-06-18 12:55:32 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(4+3);  //we are using nodejs not browser   
+
+let name = "Nanak"
+let age = 12
+let isLoggedIn = false
+
+// number range => 2 to the power 53
+//for more big number we use bigint
+//string can be use in both single and double quotation  => ""
+//boolean => true/false
+// null => standalone value
+// undefined => value not assign for now
+//symbol => unique 
+
+//object 
+
+console.log(typeof "Nanak");
+console.log(typeof age);
+
+console.log(typeof null);    //null is an object 
+console.log(typeof undefined);  //undefined is a Data Types
