32ec89f78f6bf32c4a05fbd5c15292ada678fe43
Nanak <166646358+iamNanak@users.noreply.github.com>
2024-06-20 17:41:14 +0000

    array and its mehtod

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -11,4 +11,21 @@ const dc_heros = ["SuperMan", "Flash", "BatMan"];
 
 // using Spread Operators => [...array]
 const allHeros = [...marvel_heros, ...dc_heros];
-console.log(allHeros)                         //give result same as concat but have option to add 2 or more arrays altogehter.
+// console.log(allHeros)                         //give result same as concat but have option to add 2 or more arrays altogehter.
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_another_array = another_array.flat(Infinity)    //use to spread out the array at certain given level  like 2, 3 or we can also use Infinity.
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("Nanak"));
+console.log(Array.from("Nanak"));
+console.log(Array.from({name:"Nanak"}));  // O/P=>[] because it is unable to make array, you have to tell using what symbo it convert into array
+
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
